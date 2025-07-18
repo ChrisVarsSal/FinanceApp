6af87d2d96b3b987824f998955c72ce08e26850c
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-07-18 11:18:18 -0600

    Loan
    Creation of the Loan page

---
+++ src/pages/Loans/Loan.jsx
@@ -0,0 +1,70 @@
+import "./Loan.css";
+import { useNavigate } from "react-router-dom";
+
+function Loans() {
+  const navigate = useNavigate();
+  /*const handleView = (id) => {
+    navigate(`/loan/${id}`);
+  };*/
+  return (
+    <div className="loan-container">
+      <h1>Your Loans</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Id</th>
+            <th scope="col">Name</th>
+            <th scope="col">Term (months)</th>
+            <th scope="col">Status</th>
+            <th scope="col">Expended Date</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>24</td>
+            <td>Approved</td>
+            <td>2025-01-12</td>
+            <td>
+              <button className="btn btn-view" onClick={() => navigate("/loan-info")}>
+                View
+              </button>
+            </td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            <td>@fat</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+            <td>@twitter</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button className="btn btn-secondary btn-prev">Previous</button>
+        <button className="btn btn-secondary btn-next">Next</button>
+      </div>
+      <div>
+        <button
+          className="btn btn-primary "
+          onClick={() => navigate("/new-loan")}
+        >
+          New Loan
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Loans;
