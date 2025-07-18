6401c49f89deb9d255812cc39ebdf922415f8778
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-07-18 11:19:22 -0600

    Menu
    Creation of the Menu page

---
+++ src/pages/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import "./Menu.css";
+
+function Menu() {
+  return (
+    <div className="menu">
+      <h1>Welcome to FinanceApp</h1>
+      <div className="menu-info">
+        <span>Your Financial Overview</span>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
