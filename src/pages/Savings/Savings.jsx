f83e9f856ae057015cacffc2d0f523f53ceab83a
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-07-21 22:17:26 -0600

    Savings
    Creation of the savings page

---
+++ src/pages/Savings/Savings.jsx
@@ -0,0 +1,11 @@
+import "./Savings.css";
+
+function Savings() {
+    return (
+        <div className="savings-container">
+            <h1>Welcome to the Savings Page</h1>
+        </div>
+    );
+}
+
+export default Savings;
