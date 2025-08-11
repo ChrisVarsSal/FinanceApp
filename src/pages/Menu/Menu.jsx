a27f1378e42c031ce166cdf65c6bc51313c009fa
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-08-11 11:01:44 -0600

    Update Menu.jsx
    add buttons

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,11 +1,48 @@
 import "./Menu.css";
+import { useNavigate } from "react-router-dom";
 
 function Menu() {
+  const navigate = useNavigate();
   return (
     <div className="menu">
       <h1>Welcome to FinanceApp</h1>
       <div className="menu-info">
         <span>Your Financial Overview</span>
+      </div>
+      <div className="menu-actions">
+        <button className="btn btn-primary" onClick={() => navigate("/loans")}>
+          Loans
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/new-loan")}
+        >
+         New Loan
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/savings")}
+        >
+          Savings
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/new-saving")}
+        >
+          New Saving Fund
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/dashboard")}
+        >
+          Dashboard
+        </button>
+        <button
+          className="btn btn-primary"
+          onClick={() => navigate("/documents")}
+        >
+          Documents
+        </button>
       </div>
     </div>
   );
