aad84374ce03b485b88c54029a6cfe542d643220
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-08-11 10:35:59 -0600

    Footer
    Just create the section, up to finish

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <p>&copy; 2025 FinanceApp. All rights reserved.</p>
+            <p>Developed by Your Name</p>
+        </div>
+    );
+}
+
+export default Footer;
