96f03da4857087235d5fb1885edbe11c45a0e039
ChrisVarsSal <68918398+ChrisVarsSal@users.noreply.github.com>
2025-08-22 11:46:22 -0600

    Create useCurrency.js
    creation of a hook for all the related to the currency

---
+++ src/hooks/useCurrency.js
@@ -0,0 +1,9 @@
+export function useCurrency() {
+  const formatCurrency = (amount) => {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(amount);
+  };
+  return { formatCurrency };
+}
